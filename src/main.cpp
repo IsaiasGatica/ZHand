d309ef690c0c7779bed603ded0d93553882e88d5
Isaias <isaiasgatica1@gmail.com>
2024-02-03 14:10:15 -0300

    Se conservaron solo las funciones asociadas al servomotor

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,10 @@
 #include <webServer.cpp>
 #include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_ADS1X15.h>
-#include <movingAvg.h>
 
 #define EMGRecord
 
-uint16_t adc0;
 Servo myservo;
-movingAvg avgTemp(30);
-Adafruit_ADS1115 ads;
 
 void setup()
 {
@@ -25,20 +20,10 @@ void setup()
   initWiFi();
 
   myservo.attach(16);
-  avgTemp.begin();
 
   initWebSocket();
-
   loadServer();
-
   server.begin();
-
-  if (!ads.begin())
-  {
-    Serial.println("Failed to initialize ADS.");
-    while (1)
-      ;
-  }
 }
 
 void loop()
@@ -46,27 +31,5 @@ void loop()
 
   ws.cleanupClients();
 
-  adc0 = ads.readADC_SingleEnded(0);
-  uint16_t avg = avgTemp.reading(adc0);
-  uint16_t sensorvalue = map(avg, 0, 3000, 30, 180);
-
-#ifdef EMGRecord
-  Serial.write((uint8_t *)&adc0, sizeof(adc0)); // Datos en formato binario
-#else
-  Serial.print(">AD0:");
-  Serial.println(adc0);
-  Serial.println(" ");
-  Serial.print(">Avg:");
-  Serial.println(avg);
-  Serial.println(" ");
-  Serial.print(">map:");
-  Serial.println(sensorvalue);
-  Serial.println(" ");
-#endif
-
-#ifdef EMGServo
-  myservo.write(sensorvalue);
-#else
   myservo.write(SPosicion);
-#endif
 }
